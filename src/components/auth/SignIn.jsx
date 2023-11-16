178149f0c4fcc963c48040736356102f7e1bf5bc
SabbirSami <smd71430@gmail.com>
2023-11-16 13:39:31 +0600

    change sign in page design

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -5,6 +5,7 @@ import { useContext, useState } from "react";
 import { AuthContext } from "./AuthProvider";
 import toast from "react-hot-toast";
 import { Helmet } from "react-helmet-async";
+import { BsArrowLeft } from "react-icons/bs";
 
 const SignIn = () => {
     const { signInWithGoogle, setLoading, signInUser } =
@@ -75,94 +76,106 @@ const SignIn = () => {
             });
     };
     return (
-        <div className="">
+        <div className="auth-section ">
             <Helmet>
                 <title>Gig Forge | Sign In</title>
             </Helmet>
             <div className="container mx-auto px-6">
-                <div className="grid md:grid-cols-2 grid-cols-1">
-                    <div className="py-10 flex flex-col justify-between">
-                        <h2 className="text-4xl pb-8">
-                            Hey there! <br /> Welcome back.
-                        </h2>
+                <div className="grid md:grid-cols-2 grid-cols-1 gap-14">
+                    <div className="py-20 flex flex-col justify-between bg-secondaryColor md:h-screen ">
+                        <div className="">
+                            <h2 className="text-4xl pb-8">
+                                Hey there! <br /> Welcome back.
+                            </h2>
+                            <Link className="" to={"/"}>
+                                {" "}
+                                <BsArrowLeft className="inline mb-0.5" /> Back
+                                to Home
+                            </Link>
+                        </div>
                         <div className="md:block hidden ">
                             <img src={signInImage} alt="" />
                         </div>
                     </div>
-                    <div className="bg-secondaryColor rounded-lg lg:px-16 md:px-8 lg:py-16 p-6  md:mt-16">
-                        <h2 className="text-4xl pb-8 pt-4">Sign In</h2>
-                        <form onSubmit={handleSubmit}>
-                            <label
-                                htmlFor="email"
-                                className="block md:w-64 w-full pb-2 font-semibold"
-                            >
-                                Your Email{" "}
-                                <span className="text-red-600">*</span>
-                            </label>
-                            <input
-                                type="email"
-                                name="email"
-                                required
-                                className=" rounded-md w-full py-3  px-4 bg-white/70"
-                                placeholder="Enter email here.."
-                            />
-                            <label
-                                htmlFor="password"
-                                className="block w-full pb-2  pt-8 font-semibold"
-                            >
-                                Password <span className="text-red-600">*</span>
-                            </label>
+                    <div className=" flex flex-row items-center justify-center">
+                        <div className=" rounded-lg lg:px-16 md:px-8 lg:py-16 p-6  md:mt-16 border shadow-md border-whiteSecondary/10">
+                            <h2 className="text-4xl  font-bold pb-8 pt-4">
+                                Sign In
+                            </h2>
+                            <form onSubmit={handleSubmit}>
+                                <label
+                                    htmlFor="email"
+                                    className="block lg:w-96 md:w-72 w-full pb-2 font-semibold"
+                                >
+                                    Your Email{" "}
+                                    <span className="text-red-600">*</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    required
+                                    className=" rounded-md w-full py-3 border border-whiteSecondary/40 px-4 bg-white/70"
+                                    placeholder="Enter email here.."
+                                />
+                                <label
+                                    htmlFor="password"
+                                    className="block w-full pb-2  pt-8 font-semibold"
+                                >
+                                    Password{" "}
+                                    <span className="text-red-600">*</span>
+                                </label>
 
-                            <input
-                                type="password"
-                                name="password"
-                                required
-                                className="r rounded-md w-full py-3 px-4 bg-white/70"
-                                placeholder="Enter password here.."
-                            />
-                            <label className="block w-full text-sm text-errorColor">
-                                {signInWithGoogleError}
-                            </label>
-                            <label className="block md:w-64 w-full  text-sm text-red-600">
-                                {/* {alreadyUsedEmailMessage} */}
-                            </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    required
+                                    className="r rounded-md w-full py-3 border border-whiteSecondary/40 px-4 bg-white/70"
+                                    placeholder="Enter password here.."
+                                />
+                                <label className="block w-full text-sm text-errorColor">
+                                    {signInWithGoogleError}
+                                </label>
+                                <label className="block md:w-64 w-full  text-sm text-red-600">
+                                    {/* {alreadyUsedEmailMessage} */}
+                                </label>
 
+                                <button
+                                    type="submit"
+                                    className="w-full mt-8 py-3 bg-primaryColor hover:shadow-md text-white rounded-md"
+                                >
+                                    {buttonLoading ? (
+                                        <span className="loading loading-spinner text-white"></span>
+                                    ) : (
+                                        <span>Sign In</span>
+                                    )}
+                                </button>
+                            </form>
+                            <div className="divider">OR</div>
                             <button
+                                onClick={handleSignInWithGoogle}
                                 type="submit"
-                                className="w-full mt-8 py-3 bg-primaryColor hover:shadow-md text-white rounded-md"
+                                className="w-full flex items-center justify-center gap-3 py-3 border border-primaryColor  rounded-md text-dark"
                             >
-                                {buttonLoading ? (
-                                    <span className="loading loading-spinner text-white"></span>
+                                {googleButtonLoading ? (
+                                    <span className="loading loading-spinner text-success"></span>
                                 ) : (
-                                    <span>Sign In</span>
+                                    <span className="flex items-center justify-center gap-3">
+                                        <FcGoogle className="text-2xl"></FcGoogle>
+                                        <span>Sign in with Google</span>
+                                    </span>
                                 )}
                             </button>
-                        </form>
-                        <div className="divider">OR</div>
-                        <button
-                            onClick={handleSignInWithGoogle}
-                            type="submit"
-                            className="w-full flex items-center justify-center gap-3 py-3 border border-primaryColor  rounded-md text-dark"
-                        >
-                            {googleButtonLoading ? (
-                                <span className="loading loading-spinner text-success"></span>
-                            ) : (
-                                <span className="flex items-center justify-center gap-3">
-                                    <FcGoogle className="text-2xl"></FcGoogle>
-                                    <span>Sign in with Google</span>
-                                </span>
-                            )}
-                        </button>
-                        <div className="">
-                            <p className=" pt-6">
-                                Don&#39;t have any account?{" "}
-                                <Link
-                                    className="underline text-primaryColor"
-                                    to={"/register"}
-                                >
-                                    Register
-                                </Link>
-                            </p>
+                            <div className="">
+                                <p className=" pt-6">
+                                    Don&#39;t have any account?{" "}
+                                    <Link
+                                        className="underline text-primaryColor"
+                                        to={"/register"}
+                                    >
+                                        Register
+                                    </Link>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -1,17 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../shared/navbar/Navbar";
 import Footer from "../shared/footer/Footer";
 import { Toaster } from "react-hot-toast";
 import "react-step-progress-bar/styles.css";
 
 const Root = () => {
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div>
-            <Navbar />
+            {location?.pathname === "/sign-in" ||
+                location?.pathname === "/register" || <Navbar />}
             <div className="">
                 <Outlet />
             </div>
-            <Footer />
+            {location?.pathname === "/sign-in" ||
+                location?.pathname === "/register" || <Footer />}
+
             <Toaster />
         </div>
     );

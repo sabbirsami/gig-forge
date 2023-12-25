0503f6855d9beed91e34c6dd63ea3e08304d903b
SabbirSami <smd71430@gmail.com>
2023-12-25 12:01:04 +0600

    article section done

--- src/components/home/article/Article.jsx
+++ src/components/home/article/Article.jsx
@@ -1,12 +1,15 @@
 import a01 from "../../../assets/a1.jpg";
 import a02 from "../../../assets/a04.webp";
 import a03 from "../../../assets/a05.webp";
+import { MdModeComment } from "react-icons/md";
 
 const Article = () => {
     const articles = [
         {
             title: "The Future of Remote Work",
-            image: a01,
+            description:
+                "Explore the evolving landscape of remote work and its impact on the modern workplace.",
+            image: a02,
             date: "2023-01-15",
             auth_name: "John Doe",
             auth_img: "john_doe.jpg",
@@ -15,7 +18,9 @@ const Article = () => {
         },
         {
             title: "Navigating Career Transitions",
-            image: a02,
+            description:
+                "Learn effective strategies for successfully navigating career transitions and making informed decisions.",
+            image: a01,
             date: "2023-02-10",
             auth_name: "Jane Smith",
             auth_img: "jane_smith.jpg",
@@ -23,7 +28,9 @@ const Article = () => {
             comments: 28,
         },
         {
-            title: "Tech Trends Shaping the Job Market",
+            title: "Tech Trends in the Job Market",
+            description:
+                "Discover the latest technological trends that are reshaping the job market and influencing career paths.",
             image: a03,
             date: "2023-03-05",
             auth_name: "Alex Johnson",
@@ -34,8 +41,52 @@ const Article = () => {
     ];
 
     return (
-        <div className="container mx-auto px-6">
-            <h2 className="text-4xl font-semibold">Latest Articles</h2>
+        <div className="container mx-auto px-6 py-16">
+            <h1 className="text-5xl pb-10 font-bold">Latest Articles</h1>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8">
+                {articles.map((article, idx) => (
+                    <div className="shadow-md rounded-b-lg" key={idx}>
+                        <img
+                            className="h-80 w-full object-cover rounded-t-lg"
+                            src={article.image}
+                            alt=""
+                        />
+                        <div className="lx:py-6 py-4 xl:px-10 px-6">
+                            <div className="">
+                                <h2 className="text-xl font-semibold">
+                                    {article.title}
+                                </h2>
+                                <p className="text-sm pt-2">
+                                    {article.description}
+                                </p>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex pt-6">
+                                    <div className="">
+                                        <img
+                                            className="w-10 h-10 rounded-full me-2"
+                                            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww"
+                                            alt=""
+                                        />
+                                    </div>
+                                    <div className="">
+                                        <h4 className="font-semibold">
+                                            {article.auth_name}
+                                        </h4>
+                                        <p className="text-xs">
+                                            {article.date}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="pt-4">
+                                    <MdModeComment className="text-xl inline" />{" "}
+                                    {article.comments}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
